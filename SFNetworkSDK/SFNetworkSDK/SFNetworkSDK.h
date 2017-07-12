a5089d28c88fbdd4f7ac488cfac591647431c0ee
Bharath Hariharan <bhariharan@salesforce.com>
2017-07-11 17:00:20 -0700

    Adding SFNetworkSDK project to workspace

---
+++ SFNetworkSDK/SFNetworkSDK/SFNetworkSDK.h
@@ -0,0 +1,19 @@
+//
+//  SFNetworkSDK.h
+//  SFNetworkSDK
+//
+//  Created by Bharath Hariharan on 7/11/17.
+//  Copyright © 2017 Salesforce Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SFNetworkSDK.
+FOUNDATION_EXPORT double SFNetworkSDKVersionNumber;
+
+//! Project version string for SFNetworkSDK.
+FOUNDATION_EXPORT const unsigned char SFNetworkSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SFNetworkSDK/PublicHeader.h>
+
+
